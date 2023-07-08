9dcc8fe6255173f40b61a2bf80ec8aebfba7fd3f
Kunj1311 <74526794+KUNJ1311@users.noreply.github.com>
2023-07-08 19:52:07 +0530

    --

--- Client/src/components/Admin/AdminHome/AdminHome.jsx
+++ Client/src/components/Admin/AdminHome/AdminHome.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { RiLogoutCircleLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+import { Link, animateScroll as scroll } from "react-scroll";
+import { toast } from "react-toastify";
 
 const AdminHome = () => {
-	return <div>AdminHome</div>;
+	const Navigate = useNavigate();
+	const handleLogout = () => {
+		localStorage.removeItem("coderToken");
+		Navigate("/");
+		toast.success("You have successfully logged out.");
+	};
+	return (
+		<div className="adminhome">
+			<nav className="navbar" onClick={(e) => e.stopPropagation()}>
+				<div className="nav-container">
+					<Link
+						to=""
+						onClick={() => {
+							scroll.scrollToTop();
+						}}
+						smooth={true}
+						duration={500}
+						className="nav-logo"
+					>
+						DC4 IT SOLUTIONS
+					</Link>
+					<ul>
+						<li className="p-ico">
+							<RiLogoutCircleLine className="logout" onClick={handleLogout} />
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	);
 };
 
 export default AdminHome;
